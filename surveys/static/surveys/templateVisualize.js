ca8d30de673fb7468cf1dd0821da99f5d0207bfe
Tomasz <-none>
2013-05-03 21:31:02 +0200

    Table results in a new div with rounded frames

--- surveys/static/surveys/templateVisualize.js
+++ surveys/static/surveys/templateVisualize.js
@@ -1,4 +1,4 @@
 // Run the script on DOM ready:
 $(function(){
-	$('table').visualize({type: 'bar', width: '420px'});
+	$('table').visualize({type: 'bar', width: '420px'}).appendTo('#resultsChart');
 });
